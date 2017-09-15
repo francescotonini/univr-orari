72b10db87fdbef395ece74f7789f2e30ca0dfceb
Francesco Tonini <francescoantoniotonini@gmail.com>
2017-09-15 16:51:30 +0200

    Skeleton

---
+++ univr-orari/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace univr_orari
+{
+	[Activity(Label = "univr_orari", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
